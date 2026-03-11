08e75d971f3b2e94cf5efc2f003ca8e31c46c46a
Nilesh Roy <nileshroy404@gmail.com>
2026-03-11 13:36:02 +0530

    add svg icon assets

---
+++ src/assets/svgico.js
@@ -0,0 +1,36 @@
+export const createNoteIcon = () => `
+   <svg class="inline-block w-6.5 h-6.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(173,184,194,1)">
+      <path d="M16.7574 2.99678L14.7574 4.99678H5V18.9968H19V9.23943L21 7.23943V19.9968C21 20.5491 20.5523 20.9968 20 20.9968H4C3.44772 20.9968 3 20.5491 3 19.9968V3.99678C3 3.4445 3.44772 2.99678 4 2.99678H16.7574ZM20.4853 2.09729L21.8995 3.5115L12.7071 12.7039L11.2954 12.7064L11.2929 11.2897L20.4853 2.09729Z"></path>
+   </svg>
+`
+export const searchIcon = () => `
+<svg class="inline-block w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(173,184,194,1)">
+   <path d="M11 2C15.968 2 20 6.032 20 11C20 15.968 15.968 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2ZM11 18C14.8675 18 18 14.8675 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18ZM19.4853 18.0711L22.3137 20.8995L20.8995 22.3137L18.0711 19.4853L19.4853 18.0711Z"></path>
+</svg>
+`
+export const circleIcon = () => `
+<svg class="inline-block w-3 h-3 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+   <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"></path>
+</svg>
+`
+export const deleteIcon = () => `
+<svg class="inline-block w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="none" stroke-width="1"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 4V6H15V4H9Z"></path></svg>
+`
+export const closeIcon = () => `
+<svg class="w-4 h-4 ml-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(207,207,207,1)"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg>
+`
+
+export const arrowForwardIcon = () => `
+<svg class="w-4 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgb(153 161 175)"><path d="M12 13V20L4 12L12 4V11H20V13H12Z"></path></svg>
+`
+
+export const arrowBackIcon = () => `
+<svg class="w-4 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgb(153 161 175)"><path d="M12 13H4V11H12V4L20 12L12 20V13Z"></path></svg>
+`
+
+export const editModeIcon = () => `
+<svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="rgba(173,184,194,1)"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path>
+</svg>
+`
+
+export const readOnlyIcon = () => ``
